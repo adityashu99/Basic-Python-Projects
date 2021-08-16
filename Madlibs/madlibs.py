c1e2f88b51671f912361bd758ea4d7c3ad930050
Aditya <56749986+adityashu99@users.noreply.github.com>
2021-08-16 21:53:25 +0530

    Add files via upload
    The only concept used for making this madlib is String Formatting

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,11 @@
+noun_plural = input("Noun ( Plural ) : ")
+occupation = input("Occupation : ")
+animal_plural = input("Animal ( Plural ) : ")
+place = input("Place : ")
+verb = input("Verb : ")
+madlib = f"In the book War of the {noun_plural}, the main character is an anonymous {occupation} who records \
+    the arrival of {animal_plural} in {place}. Needless to say, havoc reigns as the {animal_plural} continue \
+        to {verb} everything in sight, until they are killed by the {noun_plural}."
+print("\nMadlib : \n=========\n")
+print(madlib)
+input("\nPress any Key to Exit")
